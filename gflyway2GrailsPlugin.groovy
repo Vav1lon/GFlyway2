4ac4547ab43e48304910470fe32c4a9f869baf7e
Denis Kuzmin <d.kuzmin@acti.ru>
2013-09-17 10:31:58 +0400

    update gflyway2GrailsPlugin class

--- gflyway2GrailsPlugin.groovy
+++ gflyway2GrailsPlugin.groovy
@@ -1,50 +1,26 @@
 class gflyway2GrailsPlugin {
-    // the plugin version
+
     def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
-            , "grails-app/controllers/"
-            , "grails-app/domain/"
-            , "grails-app/i18n/"
-            , "grails-app/services/"
-            , "grails-app/taglib/"
-            , "grails-app/utils/"
-            , "grails-app/views/"
-            , "lib/"
-            , "test/"
-            , "web-app/"
     ]
 
-    // TODO Fill in these fields
-    def title = "Grails Flyway 2 Plugin" // Headline display name of the plugin
+    def title = "Grails Flyway 2 Plugin"
     def author = "Denis Kuzmin"
     def authorEmail = "vavilon.rus@gmail.com"
     def description = '''\
     Plugin flyway 2 for use in Grails
 '''
 
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/gflyway2-plugin"
+    def license = "APACHE"
 
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-    def license = "GPL2"
-
-    // Details of company behind the plugin (if there is one)
     def organization = [name: "Vav1lon", url: "http://www.vav1lon.com/"]
-
-    // Any additional developers beyond the author specified above.
-    def developers = [[name: "Kuzmin Denis", email: "vavilon.rus@gmail.com"]]
+    def developers = [[name: "Kuzmin Denis", email: "vavilon.rus@gmail.com"], [name: "Bulat Nigmatullin", email: ""]]
+    def scm = [url: "https://github.com/Vav1lon/GFlyway2"]
 
     // Location of the plugin's issue tracker.
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-    def scm = [url: "https://github.com/Vav1lon/GFlyway2"]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
