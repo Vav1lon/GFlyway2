f2be44c3fd312b3ec4b8a3893041bbeea2cf247e
Denis Kuzmin <d.kuzmin@acti.ru>
2013-09-17 10:26:01 +0400

    update gflyway2GrailsPlugin class

--- gflyway2GrailsPlugin.groovy
+++ gflyway2GrailsPlugin.groovy
@@ -19,7 +19,7 @@ class gflyway2GrailsPlugin {
     ]
 
     // TODO Fill in these fields
-    def title = "gflyway2 Plugin" // Headline display name of the plugin
+    def title = "Grails Flyway 2 Plugin" // Headline display name of the plugin
     def author = "Denis Kuzmin"
     def authorEmail = "vavilon.rus@gmail.com"
     def description = '''\
@@ -44,7 +44,7 @@ class gflyway2GrailsPlugin {
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-    def scm = [url: "http://svn.codehaus.org/grails-plugins/"]
+    def scm = [url: "https://github.com/Vav1lon/GFlyway2"]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
