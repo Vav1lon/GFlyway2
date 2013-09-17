1569aac7f6ddacd5683b028a3c689ab67126a988
Denis Kuzmin <vavilon.rus@gmail.com>
2013-09-17 22:51:16 +0400

    refactoring config

--- gflyway2GrailsPlugin.groovy
+++ gflyway2GrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class gflyway2GrailsPlugin {
 
-    def version = "0.1"
+    def version = "0.2"
     def grailsVersion = "2.2 > *"
     def pluginExcludes = [
     ]
@@ -19,37 +19,24 @@ class gflyway2GrailsPlugin {
     def developers = [[name: "Kuzmin Denis", email: "vavilon.rus@gmail.com"], [name: "Bulat Nigmatullin", email: ""]]
     def scm = [url: "https://github.com/Vav1lon/GFlyway2"]
 
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
     }
 
     def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
     }
 
     def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
     }
 
     def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
     }
 
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
     }
 
     def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 
     def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,16 +1,14 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-
 grails.project.dependency.resolution = {
-    inherits("global") {
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+    inherits 'global'
+    log 'warn'
+
     repositories {
+        grailsPlugins()
+        grailsHome()
         grailsCentral()
         mavenCentral()
     }
+
     dependencies {
         compile 'com.googlecode.flyway:flyway-core:2.2.1'
     }
