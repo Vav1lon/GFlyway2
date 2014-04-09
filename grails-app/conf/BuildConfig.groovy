70617a4301c6e9c1565e95fddad143073ebadf08
Denis Kuzmin <vavilon.rus@gmail.com>
2014-04-09 17:02:07 +0400

    up plugin version = 0.3 up version for flyway = 2.3.1
    set release plugin version =3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,7 @@
 grails.project.work.dir = 'target'
 
+grails.project.dependency.resolver = "maven"
+
 grails.project.dependency.resolution = {
 
     inherits 'global'
@@ -12,12 +14,13 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        compile 'com.googlecode.flyway:flyway-core:2.2.1'
+        compile 'com.googlecode.flyway:flyway-core:2.3.1'
     }
 
     plugins {
-        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+        build(":release:3.0.1", ":rest-client-builder:1.0.3") {
             export = false
         }
     }
+
 }
